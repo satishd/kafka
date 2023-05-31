b67b7c7af5104c8b95ab59373ae6f50ac3fbb46f
Satish Duggana <satishd@apache.org>
2023-05-31 12:08:59 +0530

    Addressed minor reveiw comments

--- core/src/main/scala/kafka/server/BrokerServer.scala
+++ core/src/main/scala/kafka/server/BrokerServer.scala
@@ -582,7 +582,7 @@ class BrokerServer(
             log.updateLogStartOffsetFromRemoteTier(remoteLogStartOffset)
           })
         },
-        brokerTopicStats));
+        brokerTopicStats))
     } else {
       None
     }
